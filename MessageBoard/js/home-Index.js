35ddb12459879ca719dea88abef6c11a31b62500
Shahab J <jamshidi.shahab@gmail.com>
2016-01-18 10:34:03 +0000

    Created Angularjs controller with TypeScript

--- MessageBoard/js/home-Index.js
+++ MessageBoard/js/home-Index.js
@@ -1,21 +1,39 @@
-﻿
-//home-Index.js
+/// <reference path="../scripts/typings/angularjs/angular.d.ts" />
+//import * as ng from 'angular';
 var module = angular.module('homeIndex', []);
-
-
-module.controller('homeIndexController', ['$scope', '$http', function ($scope, $http) {
-
-    $scope.data = [];
-    $scope.isBusy = true;
-
-    $http.get("/api/v1/topics?includeReplies=true")
-    .then(function (result) {
-        //Success
-        angular.copy(result.data, $scope.data);
-    }, function () {
-        //Error
-        Alert("Could not load topics.");
-    }).then(function () {
-        $scope.isBusy = false;
-    });
-}])
+//module.controller('homeIndexController', ['$scope', '$http', function ($scope, $http) {
+//    $scope.data = [];
+//    $scope.isBusy = true;
+//    $http.get("/api/v1/topics?includeReplies=true")
+//        .then(function (result) {
+//            //Success
+//            angular.copy(result.data, $scope.data);
+//        }, function () {
+//            //Error            
+//        }).then(function () {
+//            $scope.isBusy = false;
+//        });
+//}])
+var HomeIndexController = (function () {
+    function HomeIndexController($scope, $http) {
+        var _this = this;
+        this.$scope = $scope;
+        this.$http = $http;
+        this.data = [];
+        this.isBusy = true;
+        $http.get("/api/v1/topics?includeReplies=true")
+            .then(function (result) {
+            //Success
+            angular.copy(result.data, _this.data);
+        }, function () {
+            //Error
+        })
+            .then(function () {
+            _this.isBusy = false;
+        });
+    }
+    HomeIndexController.$inject = ['$scope', '$http'];
+    return HomeIndexController;
+})();
+module.controller('HomeIndexController', HomeIndexController);
+//# sourceMappingURL=home-Index.js.map
---
+++ MessageBoard/js/home-Index.ts
@@ -0,0 +1,44 @@
+﻿/// <reference path="../scripts/typings/angularjs/angular.d.ts" />
+//import * as ng from 'angular';
+
+
+var module = angular.module('homeIndex', []);
+
+
+//module.controller('homeIndexController', ['$scope', '$http', function ($scope, $http) {
+
+//    $scope.data = [];
+//    $scope.isBusy = true;
+
+//    $http.get("/api/v1/topics?includeReplies=true")
+//        .then(function (result) {
+//            //Success
+//            angular.copy(result.data, $scope.data);
+//        }, function () {
+//            //Error            
+//        }).then(function () {
+//            $scope.isBusy = false;
+//        });
+//}])
+
+class HomeIndexController {
+    isBusy: boolean;
+    data: ng.IHttpPromiseCallbackArg<any>;
+    static $inject = ['$scope', '$http'];
+    constructor(private $scope: ng.IScope, private $http: ng.IHttpService) {
+        this.data = [];
+        this.isBusy = true;
+        $http.get("/api/v1/topics?includeReplies=true")
+            .then((result) => {
+                //Success
+                angular.copy(result.data, this.data);
+            }, () => {
+                //Error
+            })
+            .then(() => {
+                this.isBusy = false;
+            });
+    }
+}
+
+module.controller('HomeIndexController', HomeIndexController);
